f2e4b59c37181e8f9daca402896635b2cb98023f
kcodoemena <kelechiodoemen@gmail.com>
2024-09-14 08:52:19 +0100

    added scratch

---
+++ scratch_1.py
@@ -0,0 +1,13 @@
+x = 4 #This is the starting point of execution
+while x < 11: #This is the number that x cannot get to
+    print(x) #this is what is executed
+    x += 3 #This is the number that is added to the x
+
+def greet(name):
+    print(f"Hello, {name}!")
+greet("Kelechi")
+greet("George")
+
+import math
+print(math.sqrt(16))
+math.
